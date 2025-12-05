0e4e8d673343b36f1862efbabaf4efa61507a20a
aspasiax <asxafi@gmail.com>
2025-12-05 20:49:08 +0200

    feat: add MenuApp

---
+++ src/gr/aueb/cf/MenuApp.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf;
+
+public class MenuApp {
+
+    public static void main(String[] args) {
+        System.out.println("Menu:");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+    }
+}
